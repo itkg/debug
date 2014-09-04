343230c08b3ea9595ca648a32fba9514a8287b4c
Pascal DENIS <pascal.denis@businessdecision.com>
2014-09-04 16:53:16 +0200

    add itkg/core dependency

--- src/Itkg/Debug/Composer/Installer.php
+++ src/Itkg/Debug/Composer/Installer.php
@@ -5,38 +5,45 @@ namespace Itkg\Debug\Composer;
 use Composer\Script\Event;
 
 /**
+ * Post install & update command to copy debug bar assets
+ *
  * @author Pascal DENIS <pascal.denis@businessdecision.com>
  */
 class Installer
 {
     const EXTRA_ASSET_DIR = 'itkg_debug_asset_dir';
 
+    /**
+     * Copy assets into you assets directory
+     *
+     * @param Event $event
+     */
     public static function copyAssets(Event $event)
     {
-        echo 'copy assets';
         $destination = null;
-        foreach($event->getComposer()->getRepositoryManager()->getLocalRepository()->getPackages() as $package){
-            $extra = $package->getExtra();
-            var_dump('extra : ', $extra);
-            if (isset($extra[self::EXTRA_ASSET_DIR])) {
-                $destination = $extra[self::EXTRA_ASSET_DIR];
-            }
-            break;
-        }
-
-        if (null == $destination) {
+        $extra = $event->getComposer()->getPackage()->getExtra();
+        if (!isset($extra[self::EXTRA_ASSET_DIR])) {
             return;
         }
 
-        self::copyDirectory(__DIR__.'/../Resources/*', $destination);
-        echo 'copy assets finished';
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        self::copyDirectory(
+            $vendorDir.'/maximebf/debugbar/src/DebugBar/Resources',
+            $extra[self::EXTRA_ASSET_DIR].'/vendor/maximebf/debugbar/src/DebugBar/Resources'
+        );
     }
 
+    /**
+     * Copy directory helper
+     *
+     * @param $source
+     * @param $destination
+     */
     public static function copyDirectory($source, $destination)
     {
         if (is_dir($source)) {
             if (!is_dir($destination)) {
-                mkdir($destination);
+                mkdir($destination, 0777, true);
             }
             $directory = dir($source);
             while (false !== ( $readdirectory = $directory->read())) {
