a9ad4b96fe0deba12fc853a98ebbcf0552480c46
Pascal DENIS <pascal.denis.75@gmail.com>
2014-09-18 10:49:09 +0200

    Replace copyDir by sf2 filesystem mirror function

--- src/Itkg/Debug/Composer/Installer.php
+++ src/Itkg/Debug/Composer/Installer.php
@@ -3,6 +3,7 @@
 namespace Itkg\Debug\Composer;
 
 use Composer\Script\Event;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Post install & update command to copy debug bar assets
@@ -20,43 +21,18 @@ class Installer
      */
     public static function copyAssets(Event $event)
     {
-        $destination = null;
         $extra = $event->getComposer()->getPackage()->getExtra();
         if (!isset($extra[self::EXTRA_ASSET_DIR])) {
             return;
         }
 
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-        self::copyDirectory(
+
+        $fs = new Filesystem();
+        $fs->mirror(
             $vendorDir.'/maximebf/debugbar/src/DebugBar/Resources',
+            //
             $extra[self::EXTRA_ASSET_DIR].'/vendor/maximebf/debugbar/src/DebugBar/Resources'
         );
-    }
-
-    /**
-     * Copy directory helper
-     *
-     * @param $source
-     * @param $destination
-     */
-    public static function copyDirectory($source, $destination)
-    {
-        if (!is_dir($destination)) {
-            mkdir($destination, 0777, true);
-        }
-        $directory = dir($source);
-        while (false !== ( $readdirectory = $directory->read())) {
-            if ( $readdirectory == '.' || $readdirectory == '..' ) {
-                continue;
-            }
-            $PathDir = $source . '/' . $readdirectory;
-            if (is_dir( $PathDir)) {
-                self::copyDirectory($PathDir, $destination . '/' . $readdirectory);
-                continue;
-            }
-            copy($PathDir, $destination . '/' . $readdirectory);
-        }
-
-        $directory->close();
     }
 }
