82e6ebd1968effe47e427b0e3e9c439ff27ec307
Pascal DENIS <pascal.denis@businessdecision.com>
2014-09-29 11:44:54 +0200

    Fix tests

--- tests/Itkg/Tests/Debug/DataCollector/CacheDataCollectorTest.php
+++ tests/Itkg/Tests/Debug/DataCollector/CacheDataCollectorTest.php
@@ -3,6 +3,7 @@
 namespace Itkg\Tests\Debug\DataCollector;
 
 use DebugBar\DataFormatter\DataFormatter;
+use Itkg\Core\Cache\CachableData;
 use Itkg\Core\Cache\Event\CacheEvent;
 use Itkg\Debug\DataCollector\CacheDataCollector;
 
@@ -13,9 +14,9 @@ class CacheDataCollectorTest extends \PHPUnit_Framework_TestCase
 {
     public function testCollect()
     {
-        $loadCacheEvent = new CacheEvent('my_cache_key', 86400, 'test');
-        $removeCacheEvent = new CacheEvent('my_cache_key_remove', 86400);
-        $setCacheEvent = new CacheEvent('my_cache_key_set', 86400, 'test');
+        $loadCacheEvent = new CacheEvent(new CachableData('my_cache_key', 86400, 'test'));
+        $removeCacheEvent = new CacheEvent(new CachableData('my_cache_key_remove', 86400, 'other test'));
+        $setCacheEvent = new CacheEvent(new CachableData('my_cache_key_set', 86400, 'test'));
 
         $collector = new CacheDataCollector();
 
